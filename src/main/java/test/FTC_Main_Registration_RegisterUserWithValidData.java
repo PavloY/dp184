dad7c8302dd5d8e474fed684312fee1a53b399ed
Dmitriy <zhirkod@gmail.com>
2020-03-15 20:29:28 +0200

    Use explicit wait instead of implicit in @After

--- src/main/java/test/FTC_Main_Registration_RegisterUserWithValidData.java
+++ src/main/java/test/FTC_Main_Registration_RegisterUserWithValidData.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import page.HomePage;
 import page.RegisterPage;
 import page.SuccessPage;
@@ -34,7 +36,7 @@ public class FTC_Main_Registration_RegisterUserWithValidData {
     public void registerUserWithValidDataUsingAllFields(){
         regPage.fillFirstName("Boris").fillLastName("Borisov").fillEmail("Borisov@gmail.com").fillPhone("380506526595")
                 .fillFax("8-812-1234567").fillCompany("SoftServe").fillAddress1("Kyev")
-                .fillAddress2("Peremohy Avenue, 32").fillCity("Kyiv").fillPostCode("0411687").chooseCountry("Ukraine")
+                .fillAddress2("Peremohy Avenue, 32").fillCity("Kyiv").fillPostCode("0411687541").chooseCountry("Ukraine")
                 .chooseRegion("Kyiv").fillPassword("12345").fillConfirmPassword("12345").chooseSubscribe("Yes")
                 .checkAgree();
         SuccessPage successPage = regPage.clickOnButtonWithValidData();
@@ -61,14 +63,15 @@ public class FTC_Main_Registration_RegisterUserWithValidData {
     @After
     public void tearDown(){
         driver.get("http://184-dp.tk/admin");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
         driver.findElement(By.id("input-username")).sendKeys("admin");
         driver.findElement(By.id("input-password")).sendKeys("1234566aZ$");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         WebElement link = driver.findElement(By.xpath("/html/body/div[1]/nav/ul/li[6]/a/i"));
         Actions actions = new Actions(driver);
         actions.moveToElement(link).build().perform();
-        driver.findElement(By.xpath("//li[@id='menu-customer']//ul/li/a[text()='Customers']")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Customers")));
+        driver.findElement(By.linkText("Customers")).click();
         driver.findElement(By.xpath("//tr/td[2][text()='Boris Borisov']/..//input[@name='selected[]']")).click();
         driver.findElement(By.xpath("//div[@class='pull-right']//button[@class='btn btn-danger']")).click();
         driver.switchTo().alert().accept();
