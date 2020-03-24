7a9a72d6d9aa658f37eabbadd5ca60840f49201f
Nezhenceva <anna.nezhenceva@ukr.net>
2020-03-24 14:27:42 +0200

    ProductComparisonPage and WishListPage are created for ProductPages methods.

---
+++ src/main/java/page/ProductComparisonPage.java
@@ -0,0 +1,10 @@
+package page;
+
+import core.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ProductComparisonPage extends BasePage {
+    public ProductComparisonPage(WebDriver driver) {
+        super(driver);
+    }
+}
