1c904a4953d6f399d01b96cbf4ae8c830ece91ac
Alexander Borovko <alexvonbor@gmail.com>
2020-03-25 09:13:32 +0200

    fill LogoutPage

--- src/main/java/page/LogoutPage.java
+++ src/main/java/page/LogoutPage.java
@@ -1,7 +1,23 @@
 package page;
 
-public class LogoutPage {
+import core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LogoutPage extends BasePage {
 
 
-    
+    @FindBy(xpath = "/html/body/div[2]/div/div/div/div/a")
+    private  WebElement continueButton;
+
+    public LogoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public HomePage goToHomePage() {
+        continueButton.click();
+        return super.goToHomePage();
+    }
 }
