b2df596af82a90aa710fe85a30f34459ae59334e
Nezhenceva <anna.nezhenceva@ukr.net>
2020-03-25 11:53:50 +0200

    Last changes in WishListPage

--- src/main/java/page/WishListPage.java
+++ src/main/java/page/WishListPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 
 public class WishListPage extends BasePage {
 
@@ -33,9 +31,9 @@ public class WishListPage extends BasePage {
         return this;
     }
 
-    public LoginPage continueShoppingFromWishList(){
+    public LogInPage continueShoppingFromWishList(){
         continueShoppingFromWishListButton.click();
-        return new LoginPage(driver);
+        return new LogInPage(driver);
     }
 
 }
