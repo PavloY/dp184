ef7099fcf47973cefa5736980f3ba8df05aa0133
AndrewKchn <andrewkchn@gmail.com>
2020-03-27 22:47:45 +0200

    Create search page

--- src/main/java/page/ShoppingCartPage.java
+++ src/main/java/page/ShoppingCartPage.java
@@ -8,15 +8,46 @@ import org.openqa.selenium.support.FindBy;
 public class ShoppingCartPage extends BasePage {
 
     @FindBy(xpath = "//*[contains(text(), 'Use Coupon Code')]")
-    public WebElement useCouponCode;
+    private WebElement useCoupon;
+
+    @FindBy(name = "coupon")
+    private WebElement useCouponField;
+
+    @FindBy(id = "button-coupon")
+    private WebElement applyCoupon;
+
+
 
     @FindBy(xpath = "//*[contains(text(), 'Estimate Shipping')]")
-    public WebElement estimateShipping;
+    private WebElement estimateShipping;
+
+    @FindBy(name = "country_id")
+    private WebElement country;
+
+    @FindBy(name = "zone_id")
+    private WebElement region;
+
+    @FindBy(name = "postcode")
+    private WebElement postCode;
+
+    @FindBy(id = "button-quote")
+    private WebElement getQuotesButton;
+
 
     @FindBy(xpath = "//*[contains(text(), 'Use Gift Certificate')]")
-    public WebElement useGiftCertificate;
+    private WebElement useGiftCertificate;
+
+    @FindBy(name = "voucher")
+    private WebElement giftCertificateField;
+
+    @FindBy(id = "button-voucher")
+    private WebElement applyGiftSertificateButton;
 
     public ShoppingCartPage (WebDriver driver){
         super(driver);
     }
+
+
+
+
 }
