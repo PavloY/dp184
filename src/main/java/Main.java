e55a58aa0f8b5a72b04860cd3a4cbae73561f9a2
AndrewKchn <andrewkchn@gmail.com>
2020-03-27 21:38:21 +0200

    Create search page

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import core.Config;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
 import page.HomePage;
+import page.ShoppingCartPage;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
@@ -12,11 +16,23 @@ public class Main {
         System.setProperty(Config.driver, Config.pathForDriver);
         WebDriver driver = Config.getBrowserInstance();
         driver.get(Config.site);
-        HomePage homePage = new HomePage(driver);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[4]/a")).click();
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+        shoppingCartPage.useCouponCode.click();
+//        shoppingCartPage.estimateShipping.click();
+        shoppingCartPage.useGiftCertificate.click();
+
+
 //      RegisterPage registerPage = homePage.goToRegisterPage();
 //      registerPage.chooseSubscribe("Yes");
-        homePage.chooseCurrency();
-        homePage.goToContactUsPage();
+
+
+        //driver.quit();
+
+
 
 
 
--- src/main/java/page/ShoppingCartPage.java
+++ src/main/java/page/ShoppingCartPage.java
@@ -2,8 +2,20 @@ package page;
 
 import core.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ShoppingCartPage extends BasePage {
+
+    @FindBy(xpath = "//*[contains(text(), 'Use Coupon Code')]")
+    public WebElement useCouponCode;
+
+    @FindBy(xpath = "//*[contains(text(), 'Estimate Shipping')]")
+    public WebElement estimateShipping;
+
+    @FindBy(xpath = "//*[contains(text(), 'Use Gift Certificate')]")
+    public WebElement useGiftCertificate;
+
     public ShoppingCartPage (WebDriver driver){
         super(driver);
     }
