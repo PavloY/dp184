89a829d50d623c048dc8447f6868acc3346ae923
Alexander Borovko <alexvonbor@gmail.com>
2020-03-30 12:48:40 +0300

    FTC_ContactUS_SendingMessage was added

---
+++ src/main/java/test/FTC_ContactUs_SendingMessageThroughContactUsTab.java
@@ -0,0 +1,17 @@
+package test;
+
+import core.BaseTest;
+import org.junit.Test;
+import step.ContactUsStep;
+
+public class FTC_ContactUs_SendingMessageThroughContactUsTab extends BaseTest {
+
+    ContactUsStep contactUsStep;
+
+    @Test
+    public void testSendingMessageThroughContactUsTab(){
+
+    }
+
+
+}
