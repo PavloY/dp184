64e043b434256154ca211234d7a0cc8c0efe5d9f
Alexander Borovko <alexvonbor@gmail.com>
2020-03-30 09:04:56 +0300

    fill ContactUsStep

--- src/main/java/step/ContactUsStep.java
+++ src/main/java/step/ContactUsStep.java
@@ -10,4 +10,24 @@ public class ContactUsStep extends BaseStep<ContactUsPage> {
     public ContactUsStep(WebDriver driver) {
         super(driver, new ContactUsPage(driver));
     }
+
+    public ContactUsStep fillName(String userName){
+        page.fillName(userName);
+        return this;
+    }
+
+    public ContactUsStep fillEmail(String userEmail){
+        page.fillEmail(userEmail);
+        return this;
+    }
+
+    public ContactUsStep fillEnquiry(String userEnquiry){
+        page.fillEnquiry(userEnquiry);
+        return this;
+    }
+
+    public ContactUsStep clickOnSubmitButton(){
+        page.clickOnSubmitButton();
+        return this;
+    }
 }
