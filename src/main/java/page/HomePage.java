ab4141021af958a5360e3ded06e84e7aab4833c5
Nezhenceva <anna.nezhenceva@ukr.net>
2020-03-30 07:49:32 +0300

    HomePage was updated.

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,7 +1,6 @@
 package page;
 
 import core.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,27 +11,23 @@ import java.util.List;
 
 public class HomePage extends BasePage {
     @FindBy(xpath ="//h4//a")
-    private List<WebElement> productsOnShowCase; // List
+    private List<WebElement> productsOnShowCase;
 
-//    WebElement productOnShowCase;
-//    findElement(By.xpath("//a[contains(text(), '" + productName + "')]")
+    List<String> nameProductsList = new ArrayList<>();
 
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
     public List<String> getNameProductsList() {
-        //create an empty list in which the label texts will be stored
-        List<String> nameProductsList = new ArrayList<>();
-        //iterate through all the webElements
         for (WebElement webElement : productsOnShowCase) {
             nameProductsList.add(webElement.getText());
         }
         return nameProductsList;
     }
 
-    public void clickOnProductShowCase(int number){
-        productsOnShowCase.get(number).click();
+    public void clickOnNameOfProduct(String nameOfProduct){
+        int indexOfProduct = nameProductsList.indexOf(nameOfProduct);
+        productsOnShowCase.get(indexOfProduct).click();
     }
-
 }
