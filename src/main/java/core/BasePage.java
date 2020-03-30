2446fdfd0043a429bed373a6402a069ab4e4fda0
andrewkchn <andrewkchn@gmail.com>
2020-03-30 21:59:04 +0300

    Delete empty fields

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -3,7 +3,6 @@ package core;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Locatable;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import page.*;
