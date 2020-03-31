54ca18c6b7a3be1a992311ce9ed67c3aab8a821c
Alexander Borovko <alexvonbor@gmail.com>
2020-03-31 07:42:52 +0300

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,40 @@
 import core.Config;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 
-
 import page.HomePage;
+import page.ShoppingCartPage;
+
+import java.util.concurrent.TimeUnit;
+
+import java.net.MalformedURLException;
 
 
 public class Main {
-    public static void main(String []args){
+    public static void main(String []args) throws MalformedURLException {
 
 
         System.setProperty(Config.driver, Config.pathForDriver);
         WebDriver driver = Config.getBrowserInstance();
         driver.get(Config.site);
-        HomePage homePage = new HomePage(driver);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[4]/a")).click();
+
+        ShoppingCartPage shoppingCartPage = new ShoppingCartPage(driver);
+//        shoppingCartPage.useCouponCode.click();
+//        shoppingCartPage.estimateShipping.click();
+//        shoppingCartPage.useGiftCertificate.click();
+
+
 //      RegisterPage registerPage = homePage.goToRegisterPage();
 //      registerPage.chooseSubscribe("Yes");
-        
+
+
+        //driver.quit();
+
+
 
 
 
