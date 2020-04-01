ee21c7df1df9c3ea10b3a07618202bcfa2119727
Pavlo <pavlo.yakovlev@gmail.com>
2020-04-01 21:23:22 +0300

    update property

--- src/main/java/test/FTC_Main_Login_SignInRegisteredUserWithInvalidData.java
+++ src/main/java/test/FTC_Main_Login_SignInRegisteredUserWithInvalidData.java
@@ -1,9 +1,6 @@
 package test;
 
 import core.BaseTest;
-import io.qameta.allure.Attachment;
-import io.qameta.allure.Description;
-import io.qameta.allure.Issue;
 import io.qameta.allure.Step;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,7 +12,6 @@ public class FTC_Main_Login_SignInRegisteredUserWithInvalidData extends BaseTest
   HomePageStep homePageStep;
   LoginStep loginStep;
 
-  @Step
   @Test
   public void testSignInRegisteredUserWithInvalidData() {
     homePageStep = new HomePageStep(driver);
