2a8a1e6a7fd898db724875bd8b00360d72763c64
Pavlo <pavlo.yakovlev@gmail.com>
2020-04-05 17:11:37 +0300

    Update BaseTest.java

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -34,41 +34,16 @@ import java.util.concurrent.TimeUnit;
                 logger.error("Test {} failed with {}.", method.getMethodName(), e);
             }
         };
-     //   protected RemoteWebDriver driver;
+
      protected WebDriver driver;
-    // RemoteWebDriver driver;
-/* protected WebDriver driver;
 
     @BeforeClass
     public static void start() {
-        System.setProperty(Config.driver, Config.pathForDriver);
+        System.setProperty(Config.driverName, Config.pathForDriver);
     }
-
-    @Before
-    public void setUp() throws MalformedURLException {
-        driver = Config.getBrowserInstance();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(Config.site);
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
-}
-
-
- */
-@BeforeClass
-public static void start() {
-    System.setProperty(Config.driverName, Config.pathForDriver);
-}
-
 
         @Before
-    public void setUp() throws MalformedURLException {
+    public void setUp() throws Exception {
  //     DesiredCapabilities capabilities = new DesiredCapabilities();
  //     capabilities.setBrowserName("chrome");
  //     capabilities.setCapability("enableVNC", true);
