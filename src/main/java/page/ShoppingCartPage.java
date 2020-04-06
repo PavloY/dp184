54ba1cdf8cb573db2c799f8f7d94bec817280a2c
Nezhenceva <anna.nezhenceva@ukr.net>
2020-04-06 17:57:42 +0300

    ShoppingCartPage was updated

--- src/main/java/page/ShoppingCartPage.java
+++ src/main/java/page/ShoppingCartPage.java
@@ -63,52 +63,59 @@ public class ShoppingCartPage extends BasePage {
     @FindBy(xpath = "//h1")
     private WebElement shoppingCartTitle;
 
+    @FindBy(xpath = "//a[text() = 'Checkout']")
+    private WebElement checkoutButton;
+
     private List<CartItem> items;
     private Map<String, Integer> columnName;
 
-//    @FindBy(xpath = "//*[@id='content']/form/div/table")
+    //    @FindBy(xpath = "//*[@id='content']/form/div/table")
 //    private WebElement tableItems;
-    private void createItemMap(WebElement tableItems){
+    private void createItemMap(WebElement tableItems) {
         List<WebElement> headers = tableItems.findElement(By.tagName("thead")).findElements(By.tagName("td"));
         columnName = new HashMap<>();
-        for (int i =0; i < headers.size(); i++){
-           columnName.put(headers.get(i).getText(), i);
+        for (int i = 0; i < headers.size(); i++) {
+            columnName.put(headers.get(i).getText(), i);
         }
     }
 
-    public List<CartItem> getItems(){
+    public List<CartItem> getItems() {
         WebElement tableItems = driver.findElement(By.xpath("//*[@id='content']/form/div/table"));
         createItemMap(tableItems);
         List<WebElement> items = tableItems.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
         ArrayList<CartItem> itemList = new ArrayList<>();
 
-        for (int i =0; i < items.size(); i++){
+        for (int i = 0; i < items.size(); i++) {
             List<WebElement> itemProperty = items.get(i).findElements(By.tagName("td"));
             CartItem item = new CartItem(itemProperty.get(columnName.get("Product Name")),
                     itemProperty.get(columnName.get("Unit Price")));
             itemList.add(item);
         }
         return itemList;
-       // CartItem item = CartItem.builder().name(itemProperty.get(columnName.get("Product Name"))).price(itemProperty.get(4)).build();
+        // CartItem item = CartItem.builder().name(itemProperty.get(columnName.get("Product Name"))).price(itemProperty.get(4)).build();
 //        CartItem item = new CartItem(itemProperty.get(1), itemProperty.get(4));
 
 
     }
 
-    public ShoppingCartPage (WebDriver driver){
+    public ShoppingCartPage(WebDriver driver) {
         super(driver);
     }
 
-    public String getMessageEmptyCart(){
+    public String getMessageEmptyCart() {
         return messageEmptyCart.getText();
     }
 
-    public String getNameOfProduct(){
-       return nameOfProduct.getText();
+    public String getNameOfProduct() {
+        return nameOfProduct.getText();
     }
 
-    public WebElement getShoppingCartTitle(){
+    public WebElement getShoppingCartTitle() {
         return shoppingCartTitle;
+    }
+
+    public void clickOnCheckoutButton(){
+        checkoutButton.click();
     }
 }
 
