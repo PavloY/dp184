946ec4709e5591d0119fb499b76c2d270a724706
Nezhenceva <anna.nezhenceva@ukr.net>
2020-04-06 14:08:52 +0300

    MyAccountStep returned

--- src/main/java/step/MyAccountStep.java
+++ src/main/java/step/MyAccountStep.java
@@ -4,20 +4,20 @@ import core.BaseStep;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import page.AccountPage;
-import page.ShoppingCartPage;
+
 
 public class MyAccountStep extends BaseStep<AccountPage> {
     public MyAccountStep(WebDriver driver) {
         super(driver, new AccountPage(driver));
     }
 
-//    public AddressBookStep clickOnAddressBookPageLink(){
-//        page.clickOnAddressBookPageLink();
-//        String expected = "Address Book";
-//        String actual = driver.getTitle();
-//        Assert.assertEquals(expected, actual);
-//        return new AddressBookStep(driver);
-//    }
+    public AddressBookStep clickOnAddressBookPageLink(){
+        page.clickOnAddressBookPageLink();
+        String expected = "Address Book";
+        String actual = driver.getTitle();
+        Assert.assertEquals(expected, actual);
+        return new AddressBookStep(driver);
+    }
 
     public MyAccountStep clickOnLogoutButton(){
         page.clickOnLogoutPageLink();
