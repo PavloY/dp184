b71149465393bd5a8769e2d5f03301bc913f12ec
Alexander Borovko <alexvonbor@gmail.com>
2020-04-06 18:22:34 +0300

    made a Comments User

---
+++ src/main/java/data/CommentsUser.java
@@ -0,0 +1,26 @@
+package data;
+
+public class CommentsUser {
+
+    private String name;
+    private String review;
+    private int rating;
+
+    public CommentsUser(String name, String review, int rating){
+        this.name = name;
+        this.review = review;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public int getRating(){
+        return rating;
+    }
+}
