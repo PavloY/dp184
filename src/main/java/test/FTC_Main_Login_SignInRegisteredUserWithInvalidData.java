d1b6fb2513ce8669ffc42c72577f23e9dcd62f60
Pavlo <pavlo.yakovlev@gmail.com>
2020-04-07 09:50:46 +0300

    Added Befor Method

--- src/main/java/test/FTC_Main_Login_SignInRegisteredUserWithInvalidData.java
+++ src/main/java/test/FTC_Main_Login_SignInRegisteredUserWithInvalidData.java
@@ -2,21 +2,31 @@ package test;
 
 import core.BaseTest;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import step.HomePageStep;
 import step.LoginStep;
 
+import java.net.MalformedURLException;
+
 public class FTC_Main_Login_SignInRegisteredUserWithInvalidData extends BaseTest{
 
-  HomePageStep homePageStep;
-  LoginStep loginStep;
+  private HomePageStep homePageStep;
+  private LoginStep loginStep;
+
+  @Before
+  @Override
+  public void setUp() throws MalformedURLException {
+    super.setUp();
+    homePageStep = new HomePageStep(driver);
+    loginStep = new LoginStep(driver);
+  }
+
 
   @Test
   public void testSignInRegisteredUserWithInvalidData() {
-    homePageStep = new HomePageStep(driver);
     homePageStep.clickMyAccount();
     homePageStep.clickloginDropDown();
-    loginStep = new LoginStep(driver);
     loginStep.fillLoginEmail("test");
     loginStep.fillLoginPassword("test");
     loginStep.clickLogInPageButton();
