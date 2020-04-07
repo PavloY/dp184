b04487a4165fe67c9ac9a4adf48b1254fd0d6356
Pavlo <pavlo.yakovlev@gmail.com>
2020-04-07 17:34:20 +0300

    Update LogoutStep.java

--- src/main/java/step/LogoutStep.java
+++ src/main/java/step/LogoutStep.java
@@ -1,23 +1,18 @@
 package step;
 
 import core.BaseStep;
-import data.LoginUser;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import page.HomePage;
 import page.LogoutPage;
-import page.ProductPage;
 
 public class LogoutStep extends BaseStep<LogoutPage> {
-    public LogoutStep(WebDriver driver) {
-        super(driver, new LogoutPage(driver));
-    }
+  public LogoutStep(WebDriver driver) {
+    super(driver, new LogoutPage(driver));
+  }
 
-    LogoutPage logoutPage;
+  LogoutPage logoutPage;
 
-        public LogoutStep clickContinioButton() {
-        logoutPage.clickOnContinueButton();
-        return this;
-    }
-
+  public LogoutStep clickContinioButton() {
+    logoutPage.clickOnContinueButton();
+    return this;
+  }
 }
