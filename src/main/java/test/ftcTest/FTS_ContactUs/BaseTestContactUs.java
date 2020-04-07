e7bb39bda57e5689f4662c7edd67bf7829d6f414
Alexander Borovko <alexvonbor@gmail.com>
2020-04-07 16:27:21 +0300

    refactor

--- src/main/java/test/ftcTest/FTS_ContactUs/BaseTestContactUs.java
+++ src/main/java/test/ftcTest/FTS_ContactUs/BaseTestContactUs.java
@@ -12,15 +12,14 @@ import java.net.MalformedURLException;
 public class BaseTestContactUs extends BaseTest {
 
     private ContactUsStep contactUsStep;
-    private HomePageStep homePageStep;
+    //private HomePageStep homePageStep;
 
     @Before
     @Override
     public void setUp() throws MalformedURLException {
         super.setUp();
-        homePageStep = new HomePageStep(driver);
-        contactUsStep = new ContactUsStep(driver);
-        homePageStep.clickContactUs();
+        //homePageStep = new HomePageStep(driver);
+        contactUsStep = new HomePageStep(driver).clickContactUs();
     }
 
     public ContactUsStep getContactUsStep(){
@@ -31,7 +30,7 @@ public class BaseTestContactUs extends BaseTest {
     @Override
     public void tearDown(){
         super.tearDown();
-        homePageStep = null;
+       // homePageStep = null;
         contactUsStep = null;
     }
 }
