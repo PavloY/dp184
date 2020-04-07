27e3e090ca41a90c049ad8f2363eabfc31665f0f
Alexander Borovko <alexvonbor@gmail.com>
2020-04-07 22:11:23 +0300

    refactor

--- src/main/java/data/ContactUsData.java
+++ src/main/java/data/ContactUsData.java
@@ -2,9 +2,7 @@ package data;
 
 public final class ContactUsData {
 
-    public static final String VALID_NAME = "Alexander";
-    public static final String VALID_E_MAIL = "alexvonbor@gmail.com";
-    public static final String VALID_ENQUIRY = "Hello World from automation testers ";
+
     public static final String REG_E_MAIL = "opencard@mailforspam.com";
     public static final String REG_PASSWORD = "Qwerty";
     public static final String LINK_IPHONE = "http://184-dp.tk/index.php?route=product/product&product_id=40";
