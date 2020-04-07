05c9a65ac4afe0aeda5e4c2960837c1b59edb654
Pavlo <pavlo.yakovlev@gmail.com>
2020-04-07 17:34:25 +0300

    Update test.java

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -4,8 +4,10 @@ import core.BaseTest;
 import org.junit.Test;
 import page.HomePage;
 import page.LoginPage;
+import page.LogoutPage;
 import step.HomePageStep;
 import step.LoginStep;
+import step.LogoutStep;
 
 public class test extends BaseTest {
 
@@ -13,16 +15,21 @@ public class test extends BaseTest {
   public LoginStep loginStep;
   public LoginPage loginPage;
   public HomePage homePage;
+  public LogoutStep logoutStep;
+  public LogoutPage logoutPage;
 
   @Test
   public void testSignIn() {
     homePageStep = new HomePageStep(driver);
     loginStep = new LoginStep(driver);
+    logoutStep = new LogoutStep(driver);
+    logoutPage = new LogoutPage(driver);
 
-   // loginStep.checkUserLoginExist("test_opencard@mailforspam.com", "Qwerty123456");
-  //  homePageStep.logoutExistUser();
-  //  homePage.clickMyAccount();
-  //  homePage.clickloginDropDown();
-  //  Assert.assertTrue(homePage.isUserLogIn());
+    loginStep.checkUserLoginExist("test_opencard@mailforspam.com", "Qwerty123456");
+
+    homePageStep.clickMyAccount().clickLogoutDropDown();
+    logoutPage.clickOnContinueButton();
+
+    homePageStep.isUserLogIn();
   }
 }
