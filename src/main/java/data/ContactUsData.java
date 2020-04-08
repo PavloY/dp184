57793252ac7f4d09d3150e9466806ac18b96b2b0
Alexander Borovko <alexvonbor@gmail.com>
2020-04-08 13:26:21 +0300

    update

--- src/main/java/data/ContactUsData.java
+++ src/main/java/data/ContactUsData.java
@@ -2,7 +2,7 @@ package data;
 
 public final class ContactUsData {
 
-    
+
     public static final ContactUsUser contactUsUserWithValidData = new ContactUsUser("Alex","alex@mail.com","Hello World !!!!!!!!!!!!!");
     public static final ContactUsUser contactUsUserWithInValidData = new ContactUsUser("","","");
 
