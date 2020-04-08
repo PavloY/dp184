8b1ecd7c07f843fb75bf72235bad7e55d9f975e4
Alexander Borovko <alexvonbor@gmail.com>
2020-04-08 13:16:10 +0300

    update

--- src/main/java/data/ContactUsData.java
+++ src/main/java/data/ContactUsData.java
@@ -2,11 +2,7 @@ package data;
 
 public final class ContactUsData {
 
-
-    public static final String REG_E_MAIL = "opencard@mailforspam.com";
-    public static final String REG_PASSWORD = "Qwerty";
-    public static final String LINK_IPHONE = "http://184-dp.tk/index.php?route=product/product&product_id=40";
-
+    
     public static final ContactUsUser contactUsUserWithValidData = new ContactUsUser("Alex","alex@mail.com","Hello World !!!!!!!!!!!!!");
     public static final ContactUsUser contactUsUserWithInValidData = new ContactUsUser("","","");
 
